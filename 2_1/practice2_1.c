cc79a58f5fbbcee9b7a69144ee65c0de368e7feb
sanz-jpst <akunoyoguruto@gmail.co>
2021-05-05 19:02:03 +0900

    update

---
+++ 2_1/practice2_1.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+
+int main(void)
+{
+    printf("15 + 17 = %d\n", 15 + 17);
+    
+    return 0;
+}
---
+++ 2_1/practice2_2.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+int main(void)
+{
+    printf("新\n");
+    printf("潟\n");
+    printf("大\n");
+    
+    return 0;
+}
